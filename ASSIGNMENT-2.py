2d2d4887a1801a9eca45f0c7d4ed815789a19dfc
Arham Jain <110391671+arh4m-2@users.noreply.github.com>
2024-04-27 13:52:49 +0530

    Update ASSIGNMENT-2.py

--- ASSIGNMENT-2.py
+++ ASSIGNMENT-2.py
@@ -1,3 +1,5 @@
+
+## PROG 1 : Func to check palindrome
 def palindrome(str1):
     reversed_str = str1[-1::-1]
     if reversed_str.lower() == str1.lower():
@@ -7,6 +9,28 @@ def palindrome(str1):
 
 print(palindrome("Nitin")) 
 
+
+## PROG 2 : Func for calculator
+
+def calculator(num1, num2, operation):
+    
+    if operation.lower() == 'add':
+        print(f'Addition of {num1} and {num2} : {num1+num2}')
+    if operation.lower() == 'subtraction':
+        print(f'Difference of {num1} and {num2} : {num1-num2}')
+    if operation.lower() == 'multiplication':
+        print(f'Multiplication of {num1} and {num2} : {num1*num2}')
+    if operation.lower() == 'division':
+        print(f'Division of {num1} and {num2} : {num1/num2}')
+
+x = int(input("Enter first number : "))
+y = int(input("Enter second number : "))
+z = input("Enter Operation : ")
+calculator(x,y,z)
+
+
+
+## PROG 3 : Func to count occurences of alphabets in a string
 
 def counter(str1):
     count_list = []
@@ -24,22 +48,7 @@ def counter(str1):
 counter('arham')
 
 
-def calculator(num1, num2, operation):
-    
-    if operation == 'Add':
-        print(f'Addition of {num1} and {num2} : {num1+num2}')
-    if operation == 'Subtraction':
-        print(f'Difference of {num1} and {num2} : {num1-num2}')
-    if operation == 'Multiplication':
-        print(f'Multiplication of {num1} and {num2} : {num1*num2}')
-    if operation == 'Division1':
-        print(f'Division of {num1} and {num2} : {num1/num2}')
-
-x = int(input("Enter first number : "))
-y = int(input("Enter second number : "))
-z = input("Enter Operation : ")
-calculator(x,y,z)
-
+## PROG 4 : Func to print a pattern
 
 def rightAnglePattern(n):
     for i in range(n):
@@ -51,9 +60,13 @@ x = int(input('Enter No. Of Rows : ' ))
 rightAnglePattern(x)
 
 
+
+## PROG 5 : Func to print table of a number
+
 def multiplication_table(n):
     for i in range(1,11):
         print(f'{n} x {i} = {n*i}')
 
 x = int(input('Enter a number : '))
+
 multiplication_table(x)
